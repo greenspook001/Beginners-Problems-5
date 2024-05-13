db9bf3a3723a84cf8999422e86a0df76ba06926c
greenspook001 <160692620+greenspook001@users.noreply.github.com>
2024-05-13 13:55:37 +1200

    Create beginners_problem_5.py

---
+++ beginners_problem_5.py
@@ -0,0 +1,6 @@
+# The first oroblem I did
+
+mumsList = {7 , 6, 23, 8, 18, 8, 7.2, 85, 915, 12}
+print(f"Biggest number = {max(mumsList)}")
+print(f"The avverage is = {sum(mumsList) // len(mumsList)}")
+print(f"Smallist number = {min(mumsList)}")
