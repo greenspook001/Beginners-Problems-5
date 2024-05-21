580a6fc5304235b18ff23856e6a469bcc9077e6a
greenspook001 <160692620+greenspook001@users.noreply.github.com>
2024-05-21 18:41:29 +1200

    Update beginners_problem_5.py

--- beginners_problem_5.py
+++ beginners_problem_5.py
@@ -1,10 +1,10 @@
-#The first problem I did
+# The first problem I did
 mumsList = {7 , 6, 23, 8, 18, 8, 7.2, 85, 915, 12}
 print(f"Biggest number = {max(mumsList)}")
 print(f"The avverage is = {sum(mumsList) // len(mumsList)}")
 print(f"Smallist number = {min(mumsList)}")
 
-#The second problem I did
+# The second problem I did
 stringslist = ["abc", "123", "2332", "aBBA", "heelloo", "1212", "DcEfD"]
 
 count = 0
@@ -15,7 +15,7 @@ for string in stringslist:
 
 print("Number of strings with the same character at start and end:", count)
 
-#The third problem I did
+# The third problem I did
 pesto = []
 otherfoods = []
 
@@ -32,7 +32,7 @@ for _ in range(len(pesto)):
 
 print("List of other foods: ", otherfoods)
 
-#The fourth problem I did
+# The fourth problem I did
 cereal_list = []
 
 while True:
